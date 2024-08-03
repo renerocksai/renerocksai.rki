c42f3d3ed6d3afa814290a81e350b2caa869bfc1
Rene Schallner <rene@renerocks.ai>
2024-08-03 10:55:57 +0200

    improvements - [x] color output - [x] suppress redundant context - [x] suppress redundant results - [x] context only if result is short - [x] ensure enough context (auto_context)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
-import time
-start_time  = time.time()
-
 import sys
 import os
+import time
 import faiss
 import numpy as np
 import pickle
 from embedding import EmbeddingCache
 from tqdm import tqdm
 import textwrap
+import shutil
 
 from textloading import read_text_files_by_paragraph
 from batchpacking import create_optimal_batches
@@ -71,34 +70,146 @@ def load_metadata(filepath):
         metadata = pickle.load(f)
     return metadata
 
-def show_result(index, distance, meta, width=180):
+
+def show_result(result_number, metas, result_index, distance,
+                max_filn_len=64,
+                output_width=180, 
+                num_contexts_before=1,
+                num_contexts_after=1,
+                highlight_on='>>>',
+                highlight_off='',
+                context_on='   ',
+                context_off='',
+                auto_contexts = False,
+                ):
+    result_number += 1
+    meta = metas[result_index]
+    text = meta[1]
     filp = meta[0]
     filn = os.path.basename(filp)
-    text = meta[1]
-    index += 1
-    prefix = f'#{index:02} ({distance:5.3f}): {filn:64s} | '
-    subsequent_indent = ' ' * (len(prefix) - 2) + '| '
-    line = f'{prefix}{text}'
-    wrapped_text = textwrap.fill(line, width=width, initial_indent='',
-                                 subsequent_indent=subsequent_indent)
-    print(wrapped_text)
+
+    output_tuples = []
+    if auto_contexts:
+        total_text = text
+        if auto_contexts == True:
+            desired_length = output_width
+        else:
+            desired_length = auto_contexts
+        ctx_iter = 0
+        prev_exhausted = False
+        next_exhausted = False
+        prev_ctx = []
+        next_ctx = []
+        while len(total_text) < desired_length:
+            if prev_exhausted and next_exhausted:
+                break
+            ctx_iter += 1
+            # add context before
+            delta_idx = num_contexts_before - ctx_iter
+            if result_index - delta_idx >= 0:
+                prev = metas[result_index - delta_idx]
+                # if same doc, extract text
+                if prev[0] == meta[0]:
+                    prev = prev[1]
+                    # only use if text differs from main text
+                    if prev != text:
+                        prev_ctx.append(prev)
+                        total_text += prev
+                else:
+                    prev_exhausted = True
+            # add context after
+            delta_idx = num_contexts_before + ctx_iter
+            if result_index + delta_idx < len(metas):
+                next = metas[result_index + delta_idx]
+                # if same doc, extract text
+                if next[0] == meta[0]:
+                    next = next[1]
+                    # only use if text differs from main text
+                    if next != text:
+                        next_ctx.append(next)
+                        total_text += next
+                else:
+                    next_exhausted = True
+        for prev in reversed(prev_ctx):
+            output_tuples.append((context_on, prev, context_off))
+        output_tuples.append((highlight_on, text, highlight_off))
+        for next in next_ctx:
+            output_tuples.append((context_on, next, context_off))
+    else:
+        for x in range(num_contexts_before):
+            delta_idx = num_contexts_before - x
+            if result_index - delta_idx >= 0:
+                prev = metas[result_index - delta_idx]
+                # if same doc, extract text
+                if prev[0] == meta[0]:
+                    prev = prev[1]
+                    # only use if text differs from main text
+                    if prev != text:
+                        output_tuples.append((context_on, prev, context_off))
+        output_tuples.append((highlight_on, text, highlight_off))
+        for x in range(num_contexts_after):
+            delta_idx = x
+            if result_index + delta_idx < len(metas):
+                next = metas[result_index + delta_idx]
+                # if same doc, extract text
+                if next[0] == meta[0]:
+                    next = next[1]
+                    # only use if text differs from main text
+                    if next != text:
+                        output_tuples.append((context_on, next, context_off))
+
+    fmt_filn = '{filn:' + str(max_filn_len) + 's}'
+    filn = fmt_filn.format(filn=filn)
+    for prefix, text, postfix in output_tuples:
+        wrap_prefix = f'#{result_number:02} ({distance:5.3f}): {filn} | '
+        subsequent_indent = ' ' * (len(wrap_prefix) - 2) + '| '
+        line = f'{wrap_prefix}{prefix}{text}{postfix}'
+        wrapped_text = textwrap.fill(line, width=output_width, initial_indent='',
+                                     subsequent_indent=subsequent_indent)
+        print(f'{wrapped_text}')
+
 
 def process_query(query_text, embedding_cache, faiss_index, metadata, k_results=20):
     print(f'\n=== Showing top {k_results} matches for >>>{query_text}<<< ===\n')
     query_embedding = get_query_embeddings(query_text, embedding_cache)
     query_embedding = normalize_embeddings(query_embedding)
     search_start_time = time.time()
-    distances, indices = search_faiss_index(faiss_index, query_embedding, k=k_results)
+    faiss_distances, faiss_indices = search_faiss_index(
+            faiss_index, query_embedding, k=k_results)
     search_end_time = time.time()
+    print(f"Search took {search_end_time-search_start_time:0.3f} seconds.")
 
-    # Retrieve corresponding document filenames and paragraph/table text
-    result_metadata = [metadata[i] for i in indices[0]]
-    result_distances = distances[0]
-    for index, result in enumerate(result_metadata):
-        distance = result_distances[index]
-        show_result(index, distance, result)
-    end_time = time.time()
-    print(f"Search took {search_end_time-search_start_time:0.3f} seconds. {end_time-start_time:0.3f} seconds total.")
+    result_indices = faiss_indices[0]
+    result_distances = faiss_distances[0]
+
+    max_filn_len = 0
+    for idx in result_indices:
+        filn_len = len(os.path.basename(metadata[idx][0]))
+        if filn_len > max_filn_len:
+            max_filn_len = filn_len
+
+    # get terminal width
+    terminal_size = shutil.get_terminal_size()
+    num_cols = terminal_size.columns
+
+    # compact results: supress existing identical ones, regardless of doc
+    result_texts = []
+    for r_no, (idx, dist) in enumerate(zip(result_indices, result_distances)):
+        text = metadata[idx][1]
+        if text in result_texts:
+            continue
+        result_texts.append(text)
+        show_result(r_no, metadata, idx, dist, 
+                    max_filn_len=max_filn_len,
+                    auto_contexts = True,
+                    num_contexts_before=1,
+                    num_contexts_after=1,
+                    context_on='\033[90m',
+                    context_off='\033[0m',
+                    highlight_on='\033[34m',
+                    highlight_off='\033[0m',
+                    output_width=num_cols - 1,
+                    )
 
 
 if __name__ == '__main__':
